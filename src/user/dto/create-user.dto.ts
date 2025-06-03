1a2beadcdf4b7a7341e5c43d8cef4a65f9098896
PratikPatil3235 <pratikpatil@medianv.com>
2025-06-03 15:53:21 +0530

    commits made

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { IsInt, IsString } from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
     firstName: string;
+
+    @IsString()
     lastName: string;
+
+    @IsInt()
     age: number;
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,8 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsInt, IsString } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-    firstName: string;
-    lastName: string;
-    age: number;
+  @IsString()
+  firstName: string;
+  @IsString()
+  lastName: string;
+  @IsInt()
+  age: number;
 }
